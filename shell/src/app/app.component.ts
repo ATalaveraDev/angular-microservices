50d0d76ba026eee2820fb910a95608a3335de95c
Angel <ata@loriot.io>
2023-09-23 17:10:34 +0200

    Shell - Microservice set up

---
+++ shell/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  version = VERSION.full;
+}
