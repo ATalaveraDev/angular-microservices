6bf41b10b3adcbdc24ac29607e6bf6ff63de2040
Angel Talavera <atalavera86@gmail.com>
2023-09-24 16:56:25 +0200

    Common - microservices joined

--- shell/src/app/app-routing.module.ts
+++ shell/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
+import { WebComponentWrapper, WebComponentWrapperOptions } from '@angular-architects/module-federation-tools';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: WebComponentWrapper,
+    data: {
+      remoteEntry: 'http://localhost:4202/remoteEntry.js',
+      remoteName: 'login',
+      exposedModule: './mainModule',
+      elementName: 'angular1-element'
+    } as WebComponentWrapperOptions
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
